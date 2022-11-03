a7a7e2de118b4e93e16d0f2a9eb0234f36fee68b
pauloWeslleY <weslleyspfc@13outlook.com>
2022-11-02 22:51:26 -0300

    nlw copa - aula 1 finalizada

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+interface HomeProps {
+   count: number;
+}
+
+export default function Home(props: HomeProps) {
    return (
       <section>
-         <h1>Hello NLW</h1>
+         <h1>Contagem: {props.count}</h1>
       </section>
    );
 }
+
+export const getServerSideProps = async () => {
+   const response = await fetch("http://localhost:3333/pools/count");
+   const data = await response.json();
+
+   return {
+      props: {
+         count: data.count,
+      },
+   };
+};
